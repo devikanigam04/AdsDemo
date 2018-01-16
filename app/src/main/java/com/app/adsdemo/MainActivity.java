9e624b8bde55b9da007e2fef43540f7f9b864245
Devika Nigam <devikanigam.04@gmail.com>
2018-01-16 11:55:47 +0530

    SMART_BANNER Ad with listener

--- app/src/main/java/com/app/adsdemo/MainActivity.java
+++ app/src/main/java/com/app/adsdemo/MainActivity.java
@@ -1,14 +1,17 @@
 package com.app.adsdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String TAG = this.getClass().getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,8 +20,43 @@ public class MainActivity extends AppCompatActivity {
         //Test Admob id : ca-app-pub-3940256099942544/6300978111
         MobileAds.initialize(this, "ca-app-pub-3940256099942544/6300978111");
 
+        // Banner Ad
         AdView adView = findViewById(R.id.ad_view);
         AdRequest adRequest = new AdRequest.Builder().build();
         adView.loadAd(adRequest);
+        adView.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+                Log.d(TAG, "Ad loaded!");
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // Code to be executed when an ad request fails.
+
+                Log.d(TAG, "Ad failed : "+ errorCode);
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+                Log.d(TAG, "Ad Opened!");
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+                Log.d(TAG, "User left application!");
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when when the user is about to return
+                // to the app after tapping on an ad.
+                Log.d(TAG, "Ad closed!");
+            }
+        });
     }
 }
